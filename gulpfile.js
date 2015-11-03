1b62d12118aa2a4b6a201ba7bdb5fb1523fe9141
Sean Voeller <sean.voeller@gmail.com>
2015-11-02 21:38:23 -0800

    more demonstrating task dependencies and how to fail a task

--- gulpfile.js
+++ gulpfile.js
@@ -12,3 +12,18 @@ gulp.task('monkey', function() {
 gulp.task('asdf', function(){
   console.log("another one");
 });
+
+gulp.task('one', function(cb){
+  console.log('one');
+  cb(); // NOTE: without this, 'two' won't fire!!
+})
+
+gulp.task('two', ['one'], function() {
+  console.log('two');
+});
+
+gulp.task('err', ['one'],function(cb){
+  cb("oops!");
+});
+
+gulp.task('foo',['one','two']);
