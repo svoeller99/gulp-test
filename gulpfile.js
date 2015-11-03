d8bfb2dfe693977b2d3dacac9d1b795ad6e4a44f
Sean Voeller <sean.voeller@gmail.com>
2015-11-02 21:25:04 -0800

    task dependencies

--- gulpfile.js
+++ gulpfile.js
@@ -1,6 +1,14 @@
 var gulp = require('gulp');
 
-gulp.task('default', function(){
+gulp.task('default', ['monkey','asdf'], function(){
   gulp.src('src/stuff/**/*.txt', {base:'src'})
     .pipe(gulp.dest('dest'));
+});
+
+gulp.task('monkey', function() {
+  console.log("Hello there!");
+});
+
+gulp.task('asdf', function(){
+  console.log("another one");
 });
